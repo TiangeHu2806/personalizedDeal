5bb2e9be51254f97200546d1ecfa9f2c1c3f0551
Pigeon Hu <Tiange.Hu@otto.de>
2019-02-27 15:21:48 +0100

    Init commits

---
+++ src/main/java/de/otto/personlizedDeal/PersonlizedDealApplication.java
@@ -0,0 +1,13 @@
+package de.otto.personlizedDeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonlizedDealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonlizedDealApplication.class, args);
+	}
+
+}
